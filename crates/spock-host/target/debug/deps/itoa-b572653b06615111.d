1ce8bf45154dfe489a38445eea29704f7db0fe05
Universe <universe@grida.co>
2026-07-15 05:14:42 +0900

    chore: remove generated host build artifacts

--- crates/spock-host/target/debug/deps/itoa-b572653b06615111.d
+++
@@ -1,6 +0,0 @@
-/Users/universe/Documents/GitHub/spock/crates/spock-host/target/debug/deps/itoa-b572653b06615111.d: /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/lib.rs /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/u128_ext.rs
-
-/Users/universe/Documents/GitHub/spock/crates/spock-host/target/debug/deps/libitoa-b572653b06615111.rmeta: /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/lib.rs /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/u128_ext.rs
-
-/Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/lib.rs:
-/Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/u128_ext.rs:
--- crates/spock-host/target/debug/deps/unicode_ident-122d923a082262ca.d
+++
@@ -1,8 +0,0 @@
-/Users/universe/Documents/GitHub/spock/crates/spock-host/target/debug/deps/unicode_ident-122d923a082262ca.d: /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/Users/universe/Documents/GitHub/spock/crates/spock-host/target/debug/deps/libunicode_ident-122d923a082262ca.rlib: /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/Users/universe/Documents/GitHub/spock/crates/spock-host/target/debug/deps/libunicode_ident-122d923a082262ca.rmeta: /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs:
-/Users/universe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs:
